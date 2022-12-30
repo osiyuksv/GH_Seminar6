d42b621b0c6e4323a4985597434ced8cc317d4ba
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-30 15:34:36 +0300

    Task5_CopyArray

---
+++ Task5_CopyArray/Program.cs
@@ -0,0 +1,14 @@
+﻿// выводить копию заданного массива
+// с помощью поэлементного копирования
+
+int[] array1 = {1, 5, 8, 10};
+Console.WriteLine(String.Join((", "), (array1)));
+
+int[] array2 = new int[array1.Length];
+
+for (int i = 0; i < array1.Length; i++)
+{
+  array2[i] = array1[i];
+}
+
+Console.WriteLine(String.Join((", "), (array2)));
