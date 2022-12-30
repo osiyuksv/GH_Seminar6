78f397cc4fbb9e5cc4a5095b2fbbd27259f5f62b
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-30 15:14:51 +0300

    Task4_FibonacciNnum

---
+++ Task4_FibonacciNnum/Program.cs
@@ -0,0 +1,18 @@
+﻿// вывести первые N чисел Фибоначи
+
+Console.WriteLine("Введите число элемнтов N для Фибоначи");
+int num = Convert.ToInt32(Console.ReadLine());
+ 
+ int num1 = 0;
+ int num2 = 1;
+
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+
+ for (int i = 3; i <= num; i++)
+ {
+  int NextNum = num1 + num2;
+  Console.WriteLine(NextNum);
+  num1 = num2;
+  num2 = NextNum;
+ }
