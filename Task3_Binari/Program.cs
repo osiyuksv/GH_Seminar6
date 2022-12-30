eaf00605d876525644c1fb16bb50da71056ad0b4
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-30 14:46:36 +0300

    Task3_Binari

---
+++ Task3_Binari/Program.cs
@@ -0,0 +1,15 @@
+﻿// преобразовываем дестяичное число в двоичное
+
+Console.WriteLine("Введите двоичное число N");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string str = "";
+
+while (num > 0)
+{
+  str = num % 2 + str;
+  num = num / 2;
+}
+
+
+Console.WriteLine(str);
