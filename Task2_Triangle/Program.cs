ea5453b66c1929b51cf4a4a804f54a46ac8cf125
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-30 13:31:24 +0300

    Task2_Triangle 02

--- Task2_Triangle/Program.cs
+++ Task2_Triangle/Program.cs
@@ -7,15 +7,6 @@ Console.WriteLine("Введите длину B");
 int B = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите длину C");
 int C = Convert.ToInt32(Console.ReadLine());
-int hypotenuse = A;
 
-if (A * A == C * C + B * B | B * B == A * A + C * C | C * C == A * A + B * B) 
-{
-if(A * A == C * C + B * B)Console.WriteLine("Это треугольник с гиппотенузой A = " + A);
-if(B * B == A * A + C * C) Console.WriteLine("Это треугольник с гиппотенузой B = " + B);
-if(C * C == A * A + B * B)Console.WriteLine("Это треугольник с гиппотенузой C = " + C);
-}
-
+if (A < C + B & B  < A + C & C < A + B) Console.WriteLine("Это треугольник");
 else Console.WriteLine("Это не треугольник");
-
-
